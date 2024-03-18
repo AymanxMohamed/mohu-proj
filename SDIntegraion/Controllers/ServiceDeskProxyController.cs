378610b361d92f9d325bee241b14a99a3af5e998
Menna Sobeh <Menna.Sobeh@linkdev.com>
2024-03-18 21:05:39 +0200

    reading 3 fields from Configuration

--- SDIntegraion/Controllers/ServiceDeskProxyController.cs
+++ SDIntegraion/Controllers/ServiceDeskProxyController.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using System.Text;
 using Microsoft.Net.Http.Headers;
 using System.Net;
+using MOHU.Integration.Contracts.Interface.Common;
 
 namespace SDIntegraion.Controllers
 {
@@ -13,19 +14,25 @@ namespace SDIntegraion.Controllers
     public class ServiceDeskProxyController:ControllerBase
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfigurationService _configurationservice;
 
-        public ServiceDeskProxyController(IHttpClientFactory httpClientFactory) =>
+        public ServiceDeskProxyController(IHttpClientFactory httpClientFactory , IConfigurationService configuration)
+        {
             _httpClientFactory = httpClientFactory;
+            _configurationservice = configuration;
+        }
 
         [HttpPost] 
         public async Task<object> Post(ServiceDeskRequest sdTicket)
         {
-            var username = "mohcrm";
-            var password = "P@ssword123456789";
+            var username = _configurationservice.GetConfigurationValueAsync("SD_User Name");
+            var password = _configurationservice.GetConfigurationValueAsync("SD_Password");
+            var servicedeskURL = _configurationservice.GetConfigurationValueAsync("SD_URL");
+
             string encoded = System.Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1")
                                            .GetBytes(username + ":" + password));
 
-            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "https://10.1.108.32/SM/9/rest/mohcrm");
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, servicedeskURL.ToString());
             httpRequestMessage.Headers.Add("Authorization", "Basic " + encoded);
             httpRequestMessage.Content = JsonContent.Create(sdTicket);
            
