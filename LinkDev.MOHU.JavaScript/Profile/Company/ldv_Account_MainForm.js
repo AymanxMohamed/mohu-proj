2e36fee82f31d6121143574642ae0f77c04acbd3
Alaa Ahmed <Alaa.Ahmed@linkdev.com>
2024-03-25 11:21:35 +0200

    Case Js

---
+++ LinkDev.MOHU.JavaScript/Profile/Company/ldv_Account_MainForm.js
@@ -0,0 +1,154 @@
+﻿var account = {
+    logicalName: "account",
+    ldv_contactpersonmobile: "ldv_contactpersonmobile",
+    ldv_name_ar: "ldv_name_ar",
+    name: "name",
+    ldv_contactpersonemail: "ldv_contactpersonemail",
+    telephone1: "telephone1"
+};
+
+function OnLoad(executionContext) {
+    debugger;
+    const formContext = executionContext.getFormContext();
+
+    OnLoad_MobileNumber();
+
+    //formContext.getAttribute(account.ldv_contactpersonmobile).addOnChange(function () {
+    //    ValidateMobilePhoneNumber(formContext);
+
+    //});
+    formContext.getAttribute(account.telephone1).addOnChange(function () {
+        ValidateMobilePhoneNumber(formContext);
+
+    });
+
+    formContext.getAttribute(account.name).addOnChange(function () {
+        CommonGeneric.ValidateEnglishCharacters(formContext, account.name);
+
+    });
+
+    formContext.getAttribute(account.ldv_name_ar).addOnChange(function () {
+        CommonGeneric.ValidateArabicCharacters(formContext, account.ldv_name_ar);
+
+    });
+
+    //formContext.getAttribute(account.ldv_contactpersonemail).addOnChange(function () {
+    //    ValidateEmailFormat(formContext, account.ldv_contactpersonemail);
+
+    //});
+};
+
+function OnSave(executionContext) {
+    debugger;
+    const formContext = executionContext.getFormContext();
+    var saveEvent = executionContext.getEventArgs();
+
+
+    // mobile number
+    if (
+        window.parent.intlTelInputGlobals.instances[0] != null &&
+        window.parent.intlTelInputGlobals.instances[0] != undefined
+    ) {
+        if (
+            window.parent.intlTelInputGlobals.instances[0].isValidNumber() != true &&
+            window.parent.intlTelInputGlobals.instances[0]?.getNumber() != "" &&
+            window.parent.intlTelInputGlobals.instances[0]?.getNumber() != null &&
+            window.parent.intlTelInputGlobals.instances[0]?.getNumber() != undefined
+        ) {
+            saveEvent.preventDefault();
+            ValidateMobilePhoneNumber();
+        }
+    }
+};
+
+function OnLoad_MobileNumber() {
+    debugger;
+    if (window.parent.intlTelInputGlobals)
+        if (
+            window.parent.intlTelInputGlobals.instances &&
+            (Object.keys(window.parent.intlTelInputGlobals.instances)?.length > 1 ||
+                window.parent.intlTelInputGlobals.instances?.length > 1)
+        ) {
+            var lastInstnace;
+            if (typeof window.parent.intlTelInputGlobals.instances == "object") {
+                var Instances = window.parent.intlTelInputGlobals.instances;
+                lastInstnace = Instances[Object.keys(Instances).length - 1];
+            } else {
+                window.parent.intlTelInputGlobals.instances =
+                    window.parent.intlTelInputGlobals.instances.filter(function (el) {
+                        return el != null;
+                    });
+
+                var Instances = window.parent.intlTelInputGlobals.instances;
+                lastInstnace = Instances[Instances.length - 1];
+            }
+
+            window.parent.intlTelInputGlobals.instances = [];
+            window.parent.intlTelInputGlobals.instances.push(lastInstnace);
+        }
+};
+
+
+//function ValidateMobilePhoneNumber() {
+//    debugger;
+//    var formContext = Xrm.Page;
+//    //var MobileField = formContext.getControl(account.ldv_contactpersonmobile);
+//    var MobileField = formContext.getControl(account.telephone1);
+
+//    var isvalid = window.parent.intlTelInputGlobals.instances[0].isValidNumber();
+//    if (
+//        window.parent.intlTelInputGlobals.instances[0] != null &&
+//        window.parent.intlTelInputGlobals.instances[0] != undefined &&
+//        !isvalid &&
+//        window.parent.intlTelInputGlobals.instances[0]?.getNumber() != "" &&
+//        window.parent.intlTelInputGlobals.instances[0]?.getNumber() != null &&
+//        window.parent.intlTelInputGlobals.instances[0]?.getNumber() != undefined
+//    ) {
+//        //CommonGeneric.showNotification(MobileField, "1056", "MobilePhoneValidator");
+//        CommonGeneric.showNotification(MobileField, "2", "MobilePhoneValidator");
+
+//    } else {
+//        MobileField.clearNotification("MobilePhoneValidator");
+//    }
+//};
+
+function ValidateMobilePhoneNumber() {
+    debugger;
+    var formContext = Xrm.Page;
+    var MobileField = formContext.getControl(account.telephone1);
+
+    // Check if intlTelInputGlobals.instances[0] is defined and isValidNumber function exists
+    var isvalid = window.parent.intlTelInputGlobals &&
+        window.parent.intlTelInputGlobals.instances[0] &&
+        typeof window.parent.intlTelInputGlobals.instances[0].isValidNumber === 'function' &&
+        window.parent.intlTelInputGlobals.instances[0].isValidNumber();
+
+    if (isvalid === false && window.parent.intlTelInputGlobals.instances[0]?.getNumber()) {
+        CommonGeneric.showNotification(MobileField, "2", "MobilePhoneValidator");
+    } else {
+        MobileField.clearNotification("MobilePhoneValidator");
+    }
+};
+
+function ValidateEmailFormat(formContext,fieldName) {
+    var emailValidator = /^([a-zA-Z0-9_\.\-])+@[a-zA-Z_]+?\.[a-zA-Z]{2,4}$|^([a-zA-Z0-9_\.\-])+@[a-zA-Z_]+?\.[a-zA-Z]{2,4}\.[a-zA-Z]{2,4}$/;
+    var email = formContext.getAttribute(fieldName).getValue();
+
+    if (email !== null && email !== undefined) {
+        if (!email.match(emailValidator)) {
+            //var errorMessage = "Please enter a valid email address (e.g., name@indust.com)";
+            //formContext.getControl(fieldName).setNotification(errorMessage);
+            var emailField = formContext.getControl(fieldName);
+            CommonGeneric.showNotification(emailField, "5", "EmailValidator");
+
+        } else {
+            //formContext.getControl(fieldName).clearNotification();
+            formContext.getControl(fieldName).clearNotification("EmailValidator");
+
+        }
+    } else {
+        formContext.getControl(fieldName).clearNotification("EmailValidator");
+
+    }
+};
+
---
+++ LinkDev.MOHU.JavaScript/Profile/Hajj/ldv_Hajj_MainForm.js
@@ -0,0 +1,162 @@
+﻿var hajj = {
+    LogicalName: "ldv_hajj",
+    ldv_customerid: "ldv_customerid",
+    ldv_smartcardnumber: "ldv_smartcardnumber",
+    ldv_nationalidnumberresidentidnumber: "ldv_nationalidnumberresidentidnumber",
+    ldv_visanumber: "ldv_visanumber",
+    ldv_passportnumber: "ldv_passportnumber",
+    ldv_bordernumber: "ldv_bordernumber",
+    ldv_hajjcategorycode: "ldv_hajjcategorycode",
+    ldv_hajjsid: "ldv_hajjsid",
+    ldv_hajjtype: "ldv_hajjtype",
+    ldv_hajjpermit: "ldv_hajjpermit",
+    ldv_arrivalcountry: "ldv_arrivalcountry",
+    ldv_packagesimtype: "ldv_packagesimtype",
+    ldv_reservationnumber: "ldv_reservationnumber",
+    ldv_reservationstatus: "ldv_reservationstatus",
+    ldv_sacrifices: "ldv_sacrifices",
+    ldv_internal: "ldv_internal",
+    ldv_printedpermit: "ldv_printedpermit",
+    ldv_bloodtype: "ldv_bloodtype",
+    ldv_chronicdiseases: "ldv_chronicdiseases",
+    ldv_physicaldisability: "ldv_physicaldisability",
+    ldv_hajjmedicationsused: "ldv_hajjmedicationsused",
+    ldv_hajjreceivedvaccines: "ldv_hajjreceivedvaccines",
+    ldv_expectedarrivaldate: "ldv_expectedarrivaldate",
+    ldv_arrivalflightnumber: "ldv_arrivalflightnumber",
+    ldv_flightarrivaltime: "ldv_flightarrivaltime",
+    ldv_expecteddeparturedate: "ldv_expecteddeparturedate",
+    ldv_departureflightnumber: "ldv_departureflightnumber",
+    ldv_departureflighttime: "ldv_departureflighttime",
+    ldv_insurancepolicynumber: "ldv_insurancepolicynumber",
+    ldv_insuranceactivationdate: "ldv_insuranceactivationdate",
+    ldv_insuranceexpirydate: "ldv_insuranceexpirydate",
+    ldv_totalpayment: "ldv_totalpayment",
+    ldv_availablerefundamount: "ldv_availablerefundamount",
+    ldv_companions: "ldv_companions",
+    ldv_additionalservices: "ldv_additionalservices",
+    ldv_englishname: "ldv_englishname",
+    ldv_arabicname: "ldv_arabicname",
+    ldv_nationality: "ldv_nationality",
+    ldv_gender: "ldv_gender",
+    ldv_hijribirthdate: "ldv_hijribirthdate",
+    ldv_birthdate: "ldv_birthdate",
+    ldv_contactnumber: "ldv_contactnumber",
+    ldv_email: "ldv_email",
+    ldv_companyservicenamemakkah: "ldv_companyservicenamemakkah",
+    ldv_licensenumber: "ldv_licensenumber",
+    ldv_commercialregistrationnumber: "ldv_commercialregistrationnumber",
+    ldv_companycontactnumbermakkah: "ldv_companycontactnumbermakkah",
+    ldv_servicecenterinformationmakkah: "ldv_servicecenterinformationmakkah",
+    ldv_addressandnumberofservicecentermakkah: "ldv_addressandnumberofservicecentermakkah",
+    ldv_addressnumberofservicecenterinholysites: "ldv_addressnumberofservicecenterinholysites",
+    ldv_servicecentercontactnumbermakkah: "ldv_servicecentercontactnumbermakkah",
+    ldv_companyservicenamemadina: "ldv_companyservicenamemadina",
+    ldv_companycontactnumbermadina: "ldv_companycontactnumbermadina",
+    ldv_servicecenterinformationmadina: "ldv_servicecenterinformationmadina",
+    ldv_addressservicecentermadina: "ldv_addressservicecentermadina",
+    ldv_servicecentercontactnumbermadina: "ldv_servicecentercontactnumbermadina",
+    ldv_muzdalifahhr: "ldv_muzdalifahhr",
+    ldv_muzdalifahtransport: "ldv_muzdalifahtransport",
+    ldv_muzdalifahlocation: "ldv_muzdalifahlocation",
+    ldv_mekkahguidanceservices: "ldv_mekkahguidanceservices",
+    ldv_mekkahcampname: "ldv_mekkahcampname",
+    ldv_mekkahcamplocation: "ldv_mekkahcamplocation",
+    ldv_mekkahtransport: "ldv_mekkahtransport",
+    ldv_mekkahcateringservice: "ldv_mekkahcateringservice",
+    ldv_mennacamplocationaddress: "ldv_mennacamplocationaddress",
+    ldv_mennacampnumber: "ldv_mennacampnumber",
+    ldv_mennacampcategory: "ldv_mennacampcategory",
+    ldv_mennacampservices: "ldv_mennacampservices",
+    ldv_menna_living: "ldv_menna_living",
+    ldv_mennatransport: "ldv_mennatransport",
+    ldv_madinacampmanagernumber: "ldv_madinacampmanagernumber",
+    ldv_madinacampname: "ldv_madinacampname",
+    ldv_madinacamplocation: "ldv_madinacamplocation",
+    ldv_madinacateringservice: "ldv_madinacateringservice",
+    ldv_madinatransport: "ldv_madinatransport",
+    ldv_arrafaguidanceservices: "ldv_arrafaguidanceservices",
+    ldv_arrafacamplocationaddress: "ldv_arrafacamplocationaddress",
+    ldv_arrafacampservices: "ldv_arrafacampservices",
+    ldv_arrafaliving: "ldv_arrafaliving",
+    ldv_groupname: "ldv_groupname",
+    ldv_leadersnationalid: "ldv_leadersnationalid",
+    ldv_leadersname: "ldv_leadersname",
+    ldv_leaderscontactnumber: "ldv_leaderscontactnumber",
+    createdon: "createdon",
+    createdby: "createdby",
+    modifiedon: "modifiedon",
+    modifiedby: "modifiedby",
+    ownerid: "ownerid",
+
+
+    Tabs: {
+        hajjinformation: "tab_hajjinformation",
+        serviceprovidersdetails: "tab_serviceprovidersdetails",
+        packagedetails: "tab_packagedetails",
+        administration: "tab_administration",
+        hidden:"tab_hidden"
+
+    },
+
+    Sections: {
+        healthdetails: "tab_hajjinformation_section_healthdetails",
+        flightdetails: "tab_hajjinformation_section_flightdetails",
+        details: "tab_hajjinformation_section_details",
+        hajjtype: "tab_hajjinformation_section_hajjtype",
+        payment:"tab_hajjinformation_section_payment",
+        serviceinmakkah: "tab_serviceprovidersdetails _section_ serviceinmakkah",
+        serviceinmadina: "tab_serviceprovidersdetails_section_serviceinmadina",
+        companydetails:"tab_serviceprovidersdetails_section_companydetails",
+        muzdalifa: "tab_packagedetails_section_muzdalifa",
+        menna: "tab_packagedetails_section_menna",
+        arrafa: "tab_packagedetails_section_arrafa",
+        mekkah: "tab_packagedetails_section_mekkah",
+        madina: "tab_packagedetails_section_madina",
+        leadersdetails: "tab_packagedetails_section_leadersdetails",
+        hiddenpersonaldetails:"tab_hidden_section_personaldetails",
+
+
+
+
+    },
+
+    Enums: {
+        ldv_hajjcategorycodeEnum: {
+            Internal: 1,
+            External: 2
+        }
+    }
+
+};
+
+
+
+function OnLoad (executionContext) {
+    debugger;
+    const formContext = executionContext.getFormContext();
+
+    OnChange_HajjCategory(formContext);
+    formContext.getAttribute(hajj.ldv_hajjcategorycode).addOnChange(function () {
+        OnChange_HajjCategory(formContext);
+    });
+};
+
+function OnChange_HajjCategory  (formContext) {
+    var hajjCategory = CommonGeneric.GetFieldValue(formContext, hajj.ldv_hajjcategorycode);
+    var internalFieldsToBeShown = [hajj.ldv_nationalidnumberresidentidnumber, hajj.ldv_companions, hajj.ldv_additionalservices, hajj.ldv_hajjpermit, hajj.ldv_sacrifices, hajj.ldv_internal, hajj.ldv_printedpermit];
+    var externalFieldsToBeShown = [hajj.ldv_passportnumber, hajj.ldv_visanumber, hajj.ldv_bordernumber];
+
+    // Show or hide internal fields based on permit category
+    CommonGeneric.ShowFields(formContext, internalFieldsToBeShown, hajjCategory === hajj.Enums.ldv_hajjcategorycodeEnum.Internal);
+
+    // Show or hide external fields based on permit category
+    CommonGeneric.ShowFields(formContext, externalFieldsToBeShown, hajjCategory === hajj.Enums.ldv_hajjcategorycodeEnum.External);
+
+    // Show or hide Package Details Tab based on hajj category
+    CommonGeneric.TabVisability(formContext, hajj.Tabs.packagedetails, hajjCategory === hajj.Enums.ldv_hajjcategorycodeEnum.External);
+
+    // Show or hide Flight Details Section based on hajj category
+    CommonGeneric.SectionVisibility(formContext, hajj.Tabs.hajjinformation, hajj.Sections.flightdetails, hajjCategory === hajj.Enums.ldv_hajjcategorycodeEnum.External);
+
+};
