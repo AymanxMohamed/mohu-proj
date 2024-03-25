364e3816c9539e05c567c64821547c926bb968a3
Alaa Ahmed <Alaa.Ahmed@linkdev.com>
2024-03-25 17:20:07 +0200

    Delete Instance For Default BPF Custom step

---
+++ CustomStep/LinDev.MOHU.Utilites/DeleteInstanceForDefaultBPF.cs
@@ -0,0 +1,28 @@
+﻿using LinDev.MOHU.Utilites.Logic;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinDev.MOHU.Utilites
+{
+    public class DeleteInstanceForDefaultBPF : CodeActivity
+    {
+        #region "Input Parameters"
+
+        [Input("logicalName")]
+        public InArgument<string> EntityLogicalName { get; set; }
+
+        [Input("Id")]
+        public InArgument<string> EntityId { get; set; }
+
+        #endregion
+        protected override void Execute(CodeActivityContext context)
+        {
+            new DeleteInstanceForDefaultBPFLogic().Execute(this, context);
+        }
+    }
+}
---
+++ CustomStep/LinDev.MOHU.Utilites/Logic/DeleteInstanceForDefaultBPFLogic.cs
@@ -0,0 +1,76 @@
+﻿using LinkDev.MAAN.Common;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinDev.MOHU.Utilites.Logic
+{
+    public class DeleteInstanceForDefaultBPFLogic : StepLogic<LinDev.MOHU.Utilites.DeleteInstanceForDefaultBPF>
+    {
+        protected override void ExecuteLogic()
+        {
+
+            try
+            {
+                IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+                IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+                ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+
+                // Get input parameters
+                string entityLogicalName = codeActivity.EntityLogicalName.Get(executionContext);
+                string entityIdStr = codeActivity.EntityId.Get(executionContext);
+
+                if (!string.IsNullOrEmpty(entityLogicalName) && !string.IsNullOrEmpty(entityIdStr))
+                {
+                    // Parse the entity ID
+                    //if (Guid.TryParse(entityIdStr, out Guid entityId))
+                    //{
+                        DeleteDefaultBPFInstance(entityLogicalName,  new Guid (entityIdStr));
+                    //}
+                    //else
+                    //{
+                    //    throw new Exception("Invalid entity ID format.");
+                    //}
+                }
+                else
+                {
+                    //throw new Exception("Entity logical name or ID is null or empty.");
+                    tracingService.Trace($"Entity logical name or ID is null or empty.");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                //throw new InvalidPluginExecutionException($"An error occurred: {ex.Message}");
+                tracingService.Trace($"An error occurred: {ex.Message}");
+
+            }
+        }
+
+        private void DeleteDefaultBPFInstance(string entityLogicalName, Guid entityId)
+        {
+            try
+            {
+                // Delete the default BPF instance
+                //service.Delete("processinstance", entityId);
+                //service.Delete("phonetocaseprocess", entityId);
+                service.Delete(entityLogicalName, entityId);
+                tracingService.Trace($"The Request {entityLogicalName} with id {entityId} is deleted.");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                //throw new Exception($"Error deleting default BPF instance: {ex.Message}");
+                tracingService.Trace($"Error deleting default BPF instance: {ex.Message}");
+
+            }
+        }
+    }
+}
