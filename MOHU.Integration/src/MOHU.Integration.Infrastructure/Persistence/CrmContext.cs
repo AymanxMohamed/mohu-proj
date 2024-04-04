df3ac428e74ff30592ca3b093b6b07d3e2f7b9b1
moamen_mohammed <moamen_mohammed@hotmail.com>
2024-04-04 08:44:25 +0200

    Update CRM connection string to be staging env

--- MOHU.Integration/src/MOHU.Integration.Infrastructure/Persistence/CrmContext.cs
+++ MOHU.Integration/src/MOHU.Integration.Infrastructure/Persistence/CrmContext.cs
@@ -14,9 +14,9 @@ namespace MOHU.Integration.Infrastructure.Persistence
             if (_serviceClient == null)
             {
                 var authType = "ClientSecret";
-                var clientId = "ec070189-1252-4ba0-9b8c-7fdd2477b02d";
-                var clientSecret = "eu-8Q~gah3Lph_ye.wg.IEm1pHzaU5ndY3rQnazP";
-                var url = "https://mohudev.crm4.dynamics.com/";
+                var clientId = "5476a246-c114-482e-a356-531964022997";
+                var clientSecret = "rCX8Q~cxmdBTujQAGhVYnqG35MZSqA5YCMolRdeC";
+                var url = "https://mohustg.crm4.dynamics.com/";
                 var connectionString = $"AuthType={authType};ClientId={clientId};Url={url};ClientSecret={clientSecret};";
                 _serviceClient = new ServiceClient(connectionString);
             }
