8e60421a60ab5658e7bcdff414ab5fdafa42db91
moamen_mohammed <moamen_mohammed@hotmail.com>
2024-04-04 09:16:39 +0200

    Connection string points to prod

--- MOHU.Integration/src/MOHU.Integration.Infrastructure/Persistence/CrmContext.cs
+++ MOHU.Integration/src/MOHU.Integration.Infrastructure/Persistence/CrmContext.cs
@@ -14,9 +14,9 @@ namespace MOHU.Integration.Infrastructure.Persistence
             if (_serviceClient == null)
             {
                 var authType = "ClientSecret";
-                var clientId = "ec070189-1252-4ba0-9b8c-7fdd2477b02d";
-                var clientSecret = "eu-8Q~gah3Lph_ye.wg.IEm1pHzaU5ndY3rQnazP";
-                var url = "https://mohudev.crm4.dynamics.com/";
+                var clientId = "beb8c91e-b78f-4fff-a7f4-f12a3d1b8eb9";
+                var clientSecret = "RRR8Q~WogMBTr7yJ6DylLTr72.F2GMW7LXdQfcD5";
+                var url = "https://mohu.crm4.dynamics.com/";
                 var connectionString = $"AuthType={authType};ClientId={clientId};Url={url};ClientSecret={clientSecret};";
                 _serviceClient = new ServiceClient(connectionString);
             }
