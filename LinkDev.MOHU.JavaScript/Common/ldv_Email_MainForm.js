55682a3d57b18a5e78daec592968a82f2232540c
Alaa Ahmed <Alaa.Ahmed@linkdev.com>
2024-10-28 09:42:30 +0300

    .

---
+++ LinkDev.MOHU.JavaScript/Common/ldv_Email_MainForm.js
@@ -0,0 +1,65 @@
+﻿var emailFields = {
+    from: 'from',
+    to: 'to',
+    cc: 'cc',
+    bcc: 'bcc',
+    subject: 'subject',
+    regardingobjectid: 'regardingobjectid',
+    actualdurationminutes: 'actualdurationminutes',
+    statecode:'statecode'
+
+}
+
+
+
+function OnLoad(executionContext) {
+    debugger;
+    var formContext = executionContext.getFormContext(); // get the form context
+    lockUnlockFields(formContext);
+
+    formContext.getAttribute(emailFields.statecode).addOnChange(function () {
+        lockUnlockFields(formContext);
+    });
+
+}
+
+function lockUnlockFields(formContext) {
+    var stateCode = GetFieldValue(formContext, emailFields.statecode);
+
+    // Define the fields to lock/unlock
+    var fields = [
+        emailFields.from,
+        emailFields.to,
+        emailFields.cc,
+        emailFields.bcc,
+        emailFields.subject,
+        emailFields.regardingobjectid,
+        emailFields.actualdurationminutes
+    ];
+
+    // Check if statecode is equal to 1
+    var shouldUnlock = (stateCode === 0); //Draft
+
+    // Iterate over each field and lock/unlock based on the statecode value
+    fields.forEach(function (field) {
+        var control = formContext.getControl(field);
+        if (control) {
+            control.setDisabled(!shouldUnlock); // Unlock if shouldUnlock is true, else lock
+        }
+    });
+}
+
+function GetFieldValue (formContext, fieldName) {
+    var fieldValue = null;
+
+    if (fieldName !== null) {
+        fieldAttribute = formContext.getAttribute(fieldName);
+
+        if (fieldAttribute !== null && fieldAttribute !== "undefined") {
+
+            fieldValue = fieldAttribute.getValue();
+        }
+    }
+
+    return fieldValue;
+}
