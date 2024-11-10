b5f59df0f108bbddfedd58edeeb51dea3bb63cff
AymanxMohamed <aymanmido067@gmail.com>
2024-11-10 14:43:14 +0300

    Enhancing Updates

--- MOHU.Integration/src/MOHU.Integration.Contracts/Interface/Ticket/ITicketService.cs
+++ MOHU.Integration/src/MOHU.Integration.Contracts/Interface/Ticket/ITicketService.cs
@@ -6,11 +6,15 @@ namespace MOHU.Integration.Contracts.Interface.Ticket
     public interface ITicketService
     {
         Task<TicketListResponse> GetAllTicketsAsync(Guid customerId, int pageNumber = 1, int pageSize = 10);
+        
         Task<TicketDetailsResponse> GetTicketDetailsAsync(Guid customerId, string ticketNumber);
+        
         Task<SubmitTicketResponse> SubmitTicketAsync(Guid customerId, SubmitTicketRequest request);
+        
         Task<List<TicketTypeResponse>> GetTicketTypesAsync();
+        
         Task<bool> UpdateStatusAsync(UpdateTicketStatusRequest request);
+        
         Task<Guid> GetTicketByIntegrationTicketNumberAsync(string integrationTicketNumber, string ticketNumberSchemaName);
-
     }
 }
