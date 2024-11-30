b646f0e5ca05884419b40c763e9a7ea30e097fb4
AymanxMohamed <aymanmido067@gmail.com>
2024-11-30 13:14:05 +0300

    Adding Configurations Controller

---
+++ MOHU.Integration/src/MOHU.Integration.WebApi/Features/Configurations/Controllers/ConfigurationsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MOHU.Integration.WebApi.Features.Configurations.Controllers;
+
+[Route("api/configurations")]
+public class ConfigurationsController(IConfiguration configuration) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var result = configuration.AsEnumerable().ToDictionary(kv => kv.Key, kv => kv.Value);
+        return result.Count == 0 ? ValidationProblem("No configurations available") : Ok(result);
+    }
+    
+    [HttpGet("{configurationKey}")]
+    public IActionResult Get(string configurationKey)
+    {
+        var result = configuration.GetValue<object>(configurationKey);
+        return result is null 
+            ? ValidationProblem($"No Configuration found with this key {configurationKey}") 
+            : Ok(result);
+    }
+}
