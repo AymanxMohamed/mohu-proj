728cc7ea2b480a5ab09a81ab7281f60f2c1497a4
AymanxMohamed <aymanmido067@gmail.com>
2024-11-30 14:29:12 +0300

    Adding AzureKEyVaultSettings

---
+++ MOHU.Integration/src/MOHU.Integration.WebApi/Common/Configurations/AzureKeyVault/AzureKeyVaultSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MOHU.Integration.WebApi.Common.Configurations.AzureKeyVault;
+
+public class AzureKeyVaultSettings
+{
+    public const string SectionName = nameof(AzureKeyVaultSettings);
+
+    public string VaultName { get; init; } = null!;
+}
