71b570d7c30e81a9ff1bfc1db4953ebfa053c3c2
AymanxMohamed <aymanmido067@gmail.com>
2024-12-22 09:56:43 +0300

    Adding Update Companies Request

---
+++ MOHU.Integration/src/MOHU.Integration.Contracts/Companies/Dtos/UpdateCompaniesRequest.cs
@@ -0,0 +1,24 @@
+﻿namespace MOHU.Integration.Contracts.Companies.Dtos;
+
+public record UpdateCompaniesRequest(
+    string Key,
+    int SicCode,
+    UpdateCompaniesKeyType KeyType = UpdateCompaniesKeyType.CompanyName,
+    string? NewCompanyName = null,
+    string? LicenseNumber = null);
+
+public enum UpdateCompaniesKeyType
+{
+    CompanyName,
+    Id
+}
+
+// {
+//     public string CompanyName { get; set; }
+//
+//     public string NewCompanyName { get; set; }
+//
+//     public long SicCode { get; set; }
+//
+//     public string? LicenseNumber { get; set; }
+// }
---
+++ MOHU.Integration/src/MOHU.Integration.WebApi/Features/Companies/Controllers/CompaniesControllers.cs
@@ -0,0 +1,21 @@
+﻿using MOHU.Integration.Contracts.Companies.Dtos;
+using MOHU.Integration.WebApi.Controllers;
+
+namespace MOHU.Integration.WebApi.Features.Companies.Controllers;
+
+
+[Route("api/companies")]
+public class CompaniesControllers : BaseController
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+    public IActionResult Update(UpdateCompaniesRequest request)
+    {
+        return NoContent();
+    }
+}
