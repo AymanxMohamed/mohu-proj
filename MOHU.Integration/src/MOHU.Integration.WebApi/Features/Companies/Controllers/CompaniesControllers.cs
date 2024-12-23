298877457a9d3c07a51fc4e25fbec4c26b10e47e
AymanxMohamed <aymanmido067@gmail.com>
2024-12-23 13:30:57 +0300

    Removing Companies Update End point

--- MOHU.Integration/src/MOHU.Integration.WebApi/Features/Companies/Controllers/CompaniesControllers.cs
+++ MOHU.Integration/src/MOHU.Integration.WebApi/Features/Companies/Controllers/CompaniesControllers.cs
@@ -7,14 +7,4 @@ namespace MOHU.Integration.WebApi.Features.Companies.Controllers;
 
 
 [Route("api/companies")]
-public class CompaniesControllers(ICompaniesService service) : BaseController
-{
-    [HttpPatch]
-    [ProducesResponseType((int)HttpStatusCode.NoContent)]
-    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-    public async Task<IActionResult> Update(UpdateCompaniesRequest request)
-    {
-        await service.UpdateAsync(request);
-        return NoContent();
-    }
-}
+public class CompaniesControllers(ICompaniesService service) : BaseController;
