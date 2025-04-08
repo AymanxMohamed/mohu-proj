d0c9676be8952bf82dc5f3e33971319471506186
AymanxMohamed <aymanmido067@gmail.com>
2025-04-08 15:50:20 +0300

    Adding companies service

---
+++ MOHU.Integration/src/MOHU.Integration.Application/Features/Companies/Companies/Services/CompaniesService.PopulateTeams.cs
@@ -0,0 +1,70 @@
+﻿using Common.Crm.Domain.Common.Enums;
+using Common.Crm.Domain.Common.Extensions;
+using Common.Crm.Infrastructure.Factories;
+using Common.Crm.Infrastructure.Repositories.Interfaces;
+using MOHU.Integration.Domain.Features.Companies.Constants;
+
+namespace MOHU.Integration.Application.Features.Companies.Companies.Services;
+
+public partial class CompaniesService
+{
+    private readonly IGenericRepository _genericRepository = GenericRepositoriesFactory.CreateGenericRepository(crmContext.ServiceClient);
+    
+    public Task MapDeactivatedCompaniesToNewCompanies()
+    {
+        var sourceCompanies  = GetDeactivatedCompaniesWithTeams();
+        
+        var allCompanies = GetAllSystemCompaniesThatContainsSicCode();
+        
+        MapCompanies(sourceCompanies, allCompanies);
+        
+        _genericRepository.UpdateMany(allCompanies);
+        
+        _genericRepository.Commit();
+        
+        return Task.CompletedTask;
+    }
+
+    public List<Entity> GetDeactivatedCompaniesWithTeams()
+    {
+        var query = QueryExpressionFactory.CreateQueryExpression(
+            entityLogicalName: CompaniesConstants.LogicalName,
+            conditionExpressions:
+            [
+                ConditionExpressionFactory.CreateStatusCondition(StatusEnum.InActive),
+                ConditionExpressionFactory.CreateConditionExpression(CompaniesConstants.Fields.Team,
+                    ConditionOperator.NotNull),
+                ConditionExpressionFactory.CreateConditionExpression(CompaniesConstants.Fields.SicCode,
+                    ConditionOperator.NotNull)
+            ]);
+        
+       return _genericRepository.ListAll(query).ToList();
+    }
+
+    public List<Entity> GetAllSystemCompaniesThatContainsSicCode()
+    {
+        return _genericRepository.ListAll(
+                QueryExpressionFactory.CreateQueryExpression(
+                    CompaniesConstants.LogicalName,
+                    conditionExpressions: [ConditionExpressionFactory.CreateConditionExpression(CompaniesConstants.Fields.SicCode, ConditionOperator.NotNull)]))
+            .ToList();
+    }
+
+    public static void MapCompanies(List<Entity> sourceCompanies, List<Entity> destinationCompanies)
+    {
+        sourceCompanies.MapEntitiesMatchingCriteria(
+            destinationCompanies, 
+            (sourceCompany, destinationCompany) => 
+                sourceCompany.IsAttributeEqual<string>(destinationCompany, CompaniesConstants.Fields.SicCode) && 
+                destinationCompany.Id != sourceCompany.Id);
+    }
+
+    public static void ForceMap(Entity source, Entity destination)
+    {
+        if (source.Attributes.Contains(CompaniesConstants.Fields.SicCode) &&
+            destination.Attributes.Contains(CompaniesConstants.Fields.Team))
+        {
+            destination[CompaniesConstants.Fields.Team] = source[CompaniesConstants.Fields.Team];
+        }
+    }
+}
---
+++ MOHU.Integration/src/MOHU.Integration.Application/Features/Companies/Companies/Services/CompaniesService.cs
@@ -0,0 +1,37 @@
+﻿using MOHU.Integration.Contracts.Companies.Dtos;
+using MOHU.Integration.Contracts.Companies.Services;
+using MOHU.Integration.Domain.Features.Companies;
+
+namespace MOHU.Integration.Application.Features.Companies.Companies.Services;
+
+public partial class CompaniesService(ICrmContext crmContext) : ICompaniesService
+{
+    public Task<Company> GetByElmReferenceId(long elmReferenceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(UpdateCompaniesRequest request)
+    {
+        if (request == null || request.Requests.Count == 0)
+        {
+            throw new BadRequestException("The request list is empty.");
+        }
+
+        var companies = await GetAsync(request.ToQueryExpression());
+        
+        var updatedCompanies = request.Update(companies);
+        
+        foreach (var updatedCompany in updatedCompanies)
+        {
+            await crmContext.ServiceClient.UpdateAsync(updatedCompany);
+        }
+    }
+
+    private async Task<List<Entity>> GetAsync(QueryExpression? queryExpression)
+    {
+        var companies = await crmContext.ServiceClient.RetrieveMultipleAsync(queryExpression);
+        
+        return companies?.Entities?.ToList() ?? [];
+    }
+}
