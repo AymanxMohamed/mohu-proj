80ff64e50107fe37bd0f5822bd6e1e9248596952
Emad Ramdan <Emad.Ramdan@linkdev.com>
2025-04-08 14:38:35 +0200

    add dto

---
+++ MOHU.Integration/src/MOHU.Integration.Application/Service/CategorieServices.cs
@@ -0,0 +1,58 @@
+﻿using MOHU.Integration.Contracts.Dto.CreateProfile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOHU.Integration.Application.Service
+{
+    public class CategorieServices
+    {
+        private readonly ICrmContext _crmContext;
+
+        private readonly IStringLocalizer _localizer;
+        private readonly IValidator<CreateProfileRequest> _validator;
+
+        public CategorieServices(ICrmContext crmContext,
+            IStringLocalizer localizer,
+            IValidator<CreateProfileRequest> validator)
+        {
+            _crmContext = crmContext;
+            _localizer = localizer;
+            _validator = validator;
+
+
+        }
+
+        public async Task<Guid> UpsertCategories(CreateProfileRequest model)
+        {
+            var results = await _validator.ValidateAsync(model);
+
+            if (results?.IsValid == false)
+            {
+                throw new BadRequestException(results.Errors?.FirstOrDefault()?.ErrorMessage ?? string.Empty);
+            }
+
+            var entity = new Entity(TicketCategory.EntityName);
+
+        }
+
+        private async Task<Guid?> IsCategoryExists(string TicketType, string CategoryId , int statecode)
+        {
+            // var query_statecode = 0;
+            var CategoryQuery = new QueryExpression
+            {
+                EntityName = TicketCategory.EntityName,
+                NoLock = true
+            };
+            CategoryQuery.Criteria.AddCondition("statecode", ConditionOperator.Equal, statecode);
+            CategoryQuery.Criteria.AddCondition("ldv_tickettypeid", ConditionOperator.Equal, TicketType);
+            CategoryQuery.Criteria.AddCondition("ldv_casecategoryid", ConditionOperator.Equal, CategoryId);
+
+            var response = await _crmContext.ServiceClient.RetrieveMultipleAsync(CategoryQuery);
+            return response.Entities.Count > 0 ? response?.Entities?.FirstOrDefault()?.Id : null;
+        }
+
+    }
+}
---
+++ MOHU.Integration/src/MOHU.Integration.Contracts/Dto/Category/UpsertCategoryRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOHU.Integration.Contracts.Dto.Category
+{
+    public class UpsertCategoryRequest
+    {
+        public string ArabicName { get; set; }
+
+        public string EnglishName { get; set; }
+
+        public string CategoryId { get; set; }
+
+       
+
+        public int Status { get; set; }
+
+        public string TicketType { get; set; }
+        public string ParentCategory { get; set; }
+        public string SubCategory { get; set; }
+        public string Priority { get; set; }
+        public string ComplainType { get; set; }
+        public string Season { get; set; }
+        public string SLAHourLevel1 { get; set; }
+        public string SLAHourLevel2 { get; set; }
+        public string SLAHourLevel3 { get; set; }
+
+    }
+}
